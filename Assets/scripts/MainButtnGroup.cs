8ec35f7da3be9493e47ba420474deb0e4e9242a0
Alexandria Fuller <alexfuller123@yahoo.com>
2022-10-04 03:15:15 -0400

    added main menu scene
    Added title, buttons, and Start and Quit game button functions.

---
+++ Assets/scripts/MainButtnGroup.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainButtnGroup : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        //SceneManager.LoadScene(1); //Loads by build index
+        //SceneManager.LoadScene(ScreenManager.GetActiveScene().buildIndex + 1); //Loads next scene
+        SceneManager.LoadScene("Level1"); //Loads by scene name Level1
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit(); //Quits game
+    }
+}
