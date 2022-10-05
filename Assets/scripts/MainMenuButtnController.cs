65bbe99c2f1b3adafb608b3e4e6ab0f3faf04c8d
Alexandria Fuller <alexfuller123@yahoo.com>
2022-10-05 17:45:25 -0400

    main menu script updated correctly
    Game didn't work in previous push. Updated the class in the script.

--- Assets/scripts/MainMenuButtnController.cs
+++ Assets/scripts/MainMenuButtnController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainButtnGroup : MonoBehaviour
+public class MainMenuButtnController : MonoBehaviour
 {
     public void PlayGame()
     {
