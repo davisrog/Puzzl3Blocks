21180c5d8197c516c1ed2a496e3638523daba9d8
davisrog <davisrogt@gmail.com>
2022-10-10 12:32:26 -0400

    made sendtogoogle a singleton which allows unity to actually use it
    should probably make the data collector a singleton also instead of the generic unity class

--- Assets/scripts/GoalScript.cs
+++ Assets/scripts/GoalScript.cs
@@ -2,9 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System;
 
 public class GoalScript : MonoBehaviour
 {
+    private int endmoves = 0;
+    private int enddeaths = 0;
+    private TimeSpan deltaTime;
+    //SendToGoogleForm sendtoGoogleForm;
     // Start is called before the first frame update
     private void OnTriggerEnter(Collider other)
     {
@@ -12,6 +17,16 @@ public class GoalScript : MonoBehaviour
         if (component != null)
         {
             Debug.Log("this hit?");
+            Debug.Log(component);
+            component.dataCollector.SetEndTime();
+            endmoves = component.dataCollector.GetMoves();
+            deltaTime = component.dataCollector.GetDeltaTime();
+            //deltaTime = deltaTime.ToString();
+            enddeaths = component.dataCollector.GetDeaths();
+            Debug.Log(endmoves);
+
+            SendToGoogleForm.Instance.Send(endmoves, deltaTime, enddeaths, "hate it", "no suggestions, you suck", "non-cube", "i hate puzzle games");
+
         }
     }
     }
--- Assets/scripts/SendToGoogleForm.cs
+++ Assets/scripts/SendToGoogleForm.cs
@@ -2,21 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking; //webrequest
+using System;
 
 public class SendToGoogleForm : MonoBehaviour
 {
+    
+
     private string uname = "";
     private string uage = "";
     private string ugender = "";
     private string ugameexp = "";
-    private int moves;
-    private string time;
-    private int deaths;
+   // private string likepuzzlegames = "";
+
+    //private int moves;
+    //private string timeonlevel;
+    //private int deaths;
     private string likegame;
     private string suggestions;
     private string avatar_type;
 
-    private string BASE_URL = "";  //get from google
+    private static SendToGoogleForm _instance;
+
+    public static SendToGoogleForm Instance
+    {
+        get
+        {
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+        
+    }
+
+
+    private string BASE_URL = "https://docs.google.com/forms/u/0/d/e/1FAIpQLSeB_AIXc1_scX8sK3FcIrVIBRrS2NSj8heA06DXil5QrbeccA/formResponse";  //get from google
     // Start is called before the first frame update
     void Start()
     {
@@ -33,23 +62,26 @@ public class SendToGoogleForm : MonoBehaviour
         
     }
 
-    IEnumerator Post(string uname, string uage, string ugender, string ugameexp, int moves, string time, int deaths, string likegame, string suggestions, string avatar_type)
+    //IEnumerator Post(string uname, string uage, string ugender, string ugameexp, int moves, string timeonlevel, int deaths, string likegame, string suggestions, string avatar_type, string likepuzzlegames)
+    IEnumerator Post(int moves, TimeSpan timeonlevel, int deaths, string likegame, string suggestions, string avatar_type, string likepuzzlegames)
     {
         WWWForm form = new WWWForm();
+       // timeonlevel = timeonlevel.ToString(@"hh\:mm\:ss");
         //dependent variables the user provides
-        form.AddField("", uname);
-        form.AddField("", uage);
-        form.AddField("", ugender);
-        form.AddField("", ugameexp);
+        form.AddField("entry.911002939", uname);
+        form.AddField("entry.96120943", uage);
+        form.AddField("entry.776381412", ugender);
+        form.AddField("entry.59901152", ugameexp);
+        form.AddField("entry.1004050490", likepuzzlegames);
         //game tracks
-        form.AddField("", moves);
-        form.AddField("", time);
-        form.AddField("", deaths);
+        form.AddField("entry.1234682582", moves);
+       // form.AddField("entry.1738161824", timeonlevel);
+        form.AddField("entry.1758611915", deaths);
 
         //user provides after playing
-        form.AddField("", likegame);
-        form.AddField("", suggestions);
-        form.AddField("", avatar_type);
+        form.AddField("entry.1147099413", likegame);
+        form.AddField("entry.1912895330", suggestions);
+        form.AddField("entry.746764486", avatar_type);
 
 
 
@@ -69,10 +101,14 @@ public class SendToGoogleForm : MonoBehaviour
         www.Dispose(); //have to dispose this or we get a memory leak
     }
 
-    public void Send()
+    public void Send(int moves, TimeSpan timeonlevel, int deaths, string likegame, string suggestions, string avatar_type, string likepuzzlegames)
     {
         //teststring = test.GetComponent<InputField>().text;
-
-        StartCoroutine(Post(uname, uage,  ugender,  ugameexp,  moves,  time,  deaths,  likegame,  suggestions, avatar_type));
+        /* moves = dataCollector.GetMoves();
+         deaths = dataCollector.GetDeaths();
+         timeonlevel = dataCollector.GetDeltaTime();
+ */
+        StartCoroutine(Post(moves, timeonlevel, deaths, likegame, suggestions, avatar_type, likepuzzlegames));
+       // StartCoroutine(Post(uname, uage, ugender, ugameexp, moves, timeonlevel, deaths, likegame, suggestions, avatar_type, likepuzzlegames));
     }
 }
